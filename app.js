77e5c0244f4f37fc865fc5f978277dde2ebdad09
Medine Hemidova <maddyhamidova2010@gmail.com>
2024-12-24 23:45:52 +0400

    array-1

---
+++ app.js
@@ -0,0 +1,37 @@
+// 1. Üç qrupun adlarından massiv yaradın; Başqa bir qrup yoldaşının adını massivin sonuna əlavə edin; İlk adı silin; Massivi konsola çıxarın.
+// const students = ["nergiz", "medine", "yehya"]
+// students.push("seccad")
+// students.shift()
+// console.log(students);
+
+
+// 2. Addan daxil massiv yaradın. Massivin ikinci elementinin dəyərini “Classified” ilə əvəz edin. Massivi konsola çıxarın.
+// const users = ["Aydan", "Caydan", "Caynik"];
+// const newUsers = users.splice(1, 1, "Classified")
+// console.log(users);
+
+
+// 3. Kütləvi verilib. "Episode 4: New Hope" kimi mesajları olaraq konsola çıxarın. Massiv elementlərin düzün.
+// let arr = ['Yeni Ümid', 'İmperiya geri qayıdır', 'Jdi'nin qayıdışı'];
+
+// //Episode 4: New Hope
+// //Episode 5: Empire strikes back
+// //Episode 6: Return of the Jdi
+// const arr = ["New Hope", "The Empire Strikes Back", "Return of the Jdi"];
+// let episode = 4
+// for (let x in arr) {
+//     console.log(`Episode ${+x + 4}: ${arr[x]}`);
+// }
+
+
+// 4.İstifadəçidən vergüllə ayrılmış beş ədəd daxil olmaq xahiş edin və onları kütləvi şəkildə qeyd edin. İstifadəçidən beş ədəd elementi daxil etmək tələb olunur, onları massivə yazan və ən kiçik ədədi konsola çıxaran proqram yazın.
+// const numbers = prompt("5 eded daxil edin, ededlerin vergul ile ayirin")
+// const arr = numbers.split(",")
+// const smallNumber = Math.min(...arr)
+// console.log(smallNumber);
+
+
+// 5. Verilən mətn tip dəyərindəki vergülləri nöqtəli vergüllə əvəz edin. '32, 31, 34, 36, 31' mətni verilmişdir ,İçindəki vergülləri nöqtəli vergüllə əvəz edin.(Massivden istifadə etməklə.
+// const str = "12, 44, 35, 57, 76, 90, 32"
+// const str2 = str.replaceAll(",", ";")
+// console.log(str2);
